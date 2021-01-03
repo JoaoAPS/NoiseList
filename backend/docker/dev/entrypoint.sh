2719b58770ca33b397e498929f5ebedf617f6a55
João Paludo <jas.paludo@gmail.com>
2021-01-03 13:34:26 -0300

    Set up initial database

--- backend/docker/dev/entrypoint.sh
+++ backend/docker/dev/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ ! -f /backend/data/db.sqlite3 ]; then
 	cp /backend/data/initial_db.sqlite3 /backend/data/db.sqlite3
-	python manage.py migrate
 fi
 
-gunicorn backend.wsgi -b 0.0.0.0:7000
+python manage.py migrate
+python manage.py runserver 0.0.0.0:7000
